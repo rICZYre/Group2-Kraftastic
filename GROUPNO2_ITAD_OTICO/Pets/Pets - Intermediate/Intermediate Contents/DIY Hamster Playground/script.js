90803e16c213dc8d6dbcc680efdd232021320a42
abeabo30 <oticogabriel20@gmail.com>
2024-05-05 22:01:44 +0800

    .

---
+++ GROUPNO2_ITAD_OTICO/Pets/Pets - Intermediate/Intermediate Contents/DIY Hamster Playground/script.js
@@ -0,0 +1,43 @@
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+searchBox.addEventListener("click", ()=>{
+  navbar.classList.toggle("showInput");
+  if(navbar.classList.contains("showInput")){
+    searchBox.classList.replace("bx-search" ,"bx-x");
+  }else {
+    searchBox.classList.replace("bx-x" ,"bx-search");
+  }
+});
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let EducationArrow = document.querySelector(".Education-arrow");
+EducationArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let FashionArrow = document.querySelector(".Fashion-arrow");
+FashionArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let HomeArrow = document.querySelector(".Home-arrow");
+HomeArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
+let PetsArrow = document.querySelector(".Pets-arrow");
+PetsArrow.onclick = function() {
+ navLinks.classList.toggle("show4");
+}
